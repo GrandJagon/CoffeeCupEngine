d731052a481fc47c2e760aa1df262aa1bee6b7cb
GrandJagon <lucas.noirot@outlook.fr>
2023-11-01 17:12:25 +0100

    Adding SDL2 includes and lib directories in CMAKE file

--- CoffeeCupEngine/src/platform/OpenGL/GlUtils.h
+++ CoffeeCupEngine/src/platform/OpenGL/GlUtils.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <GL/glew.h>
-#include <GL/glut.h>
+#include "GL/glew.h"
+#include "GL/glut.h"
 
-#define ASSERT(x) if(!x) __builtin_trap()
-#define GlCall(x) GlClearError(); \
-        x;\
-        ASSERT(GlLogError(#x, __FILE__,__LINE__));
-
+#define ASSERT(x) \
+        if (!x)   \
+        __builtin_trap()
+#define GlCall(x)       \
+        GlClearError(); \
+        x;              \
+        ASSERT(GlLogError(#x, __FILE__, __LINE__));
 
 // Clears all the error in the openGl error cache
 // Useful to retrieve an actual error as it can be some noise
@@ -15,8 +17,4 @@ void GlClearError();
 
 // Does nohing and returns true if there si no error
 // Prints it and returnfs false if there is an error
-bool GlLogError(const char* function, const char* file, int line);
-
-
-
-
+bool GlLogError(const char *function, const char *file, int line);
--- CoffeeCupEngine/src/rendering/camera.h
+++ CoffeeCupEngine/src/rendering/camera.h
@@ -1,36 +1,35 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
-
-class Camera 
+class Camera
 {
-    private: 
-        glm::vec3 _position;
+private:
+    glm::vec3 _position;
 
-        float _speed = 5;
+    float _speed = 5;
 
-        // Adjusts coordinates to screen coordinates
-        glm::mat4 _projectionMatrix;
+    // Adjusts coordinates to screen coordinates
+    glm::mat4 _projectionMatrix;
 
-        // Sets what camera sees 
-        glm::mat4 _viewMatrix;
+    // Sets what camera sees
+    glm::mat4 _viewMatrix;
 
-        // Product of both to be given to shader
-        glm::mat4 _viewProjectionMatrix;
+    // Product of both to be given to shader
+    glm::mat4 _viewProjectionMatrix;
 
-        void translate();
-    
-    public:
-        Camera(float left, float right, float bottom, float top);
-    
-        ~Camera() = default;
+    void translate();
 
-        glm::mat4 getVPM() { return _viewProjectionMatrix; };
+public:
+    Camera(float left, float right, float bottom, float top);
 
-        void setSpeed(float newSpeed) { _speed = newSpeed; };
+    ~Camera() = default;
 
-        void move(glm::vec3 translation);
-        void move(float x, float y, float z = 0);
+    glm::mat4 getVPM() { return _viewProjectionMatrix; };
+
+    void setSpeed(float newSpeed) { _speed = newSpeed; };
+
+    void move(glm::vec3 translation);
+    void move(float x, float y, float z = 0);
 };
